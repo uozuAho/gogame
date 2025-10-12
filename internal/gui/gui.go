f62775047e78d611274e32a824eebb9382ac1c49
me <me@me.com>
2025-10-12 14:17:50 +1100

    WIP: sound on shoot

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -1,31 +1,58 @@
 package gui
 
 import (
+	"bytes"
 	"log"
 	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
 
 	"mygame/internal/game"
 )
 
 type GameAdapter struct {
-	game           *game.Game
-	dudeRenderer   *DudeRenderer
-	bulletRenderer *BulletRenderer
+	game              *game.Game
+	dudeRenderer      *DudeRenderer
+	bulletRenderer    *BulletRenderer
+	audioCtx          *audio.Context
+	shootWav          []byte
+	prevMouseLeftDown bool
 }
 
 func (adpt *GameAdapter) Update() error {
+	// detect mouse click edge so we can play a sound when the user shoots
 	cursorPosX, cursorPosY := ebiten.CursorPosition()
+	mouseDown := ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft)
+	if mouseDown && !adpt.prevMouseLeftDown && adpt.audioCtx != nil && len(adpt.shootWav) > 0 {
+		// Play the shot sound in a goroutine so we don't block the UI/update loop
+		go func() {
+			r := bytes.NewReader(adpt.shootWav)
+			s, err := wav.Decode(adpt.audioCtx, r)
+			if err != nil {
+				log.Printf("failed to decode shoot wav: %v", err)
+				return
+			}
+			player, err := audio.NewPlayer(adpt.audioCtx, s)
+			if err != nil {
+				log.Printf("failed to create audio player: %v", err)
+				return
+			}
+			player.Play()
+		}()
+	}
+
 	inputs := game.GameInput{
 		LeftPressed:   ebiten.IsKeyPressed(ebiten.KeyA),
 		RightPressed:  ebiten.IsKeyPressed(ebiten.KeyD),
 		DownPressed:   ebiten.IsKeyPressed(ebiten.KeyS),
 		UpPressed:     ebiten.IsKeyPressed(ebiten.KeyW),
-		MouseLeftDown: ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft),
+		MouseLeftDown: mouseDown,
 		CursorPos:     game.Point2D{X: float64(cursorPosX), Y: float64(cursorPosY)},
 	}
 	adpt.game.Update(&inputs)
+	adpt.prevMouseLeftDown = mouseDown
 	return nil
 }
 
@@ -60,6 +87,10 @@ func RunGui(game *game.Game) {
 		dudeRenderer:   &dudeRenderer,
 		bulletRenderer: &bulletRenderer,
 	}
+	// initialize audio context and in-memory shot sound
+	const sampleRate = 44100
+	adapter.audioCtx = audio.NewContext(sampleRate)
+	adapter.shootWav = GenerateShootWavBytes()
 	ebiten.SetWindowSize(1024, 768)
 	ebiten.SetWindowTitle("mygame")
 	if err := ebiten.RunGame(&adapter); err != nil {
