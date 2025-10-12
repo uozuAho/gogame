d034d6910622ef199e7d63746ce54555849d26a6
me <me@me.com>
2025-10-12 17:09:01 +1100

    rm red line

--- internal/gui/dude_renderer.go
+++ internal/gui/dude_renderer.go
@@ -1,13 +1,11 @@
 package gui
 
 import (
-	"image/color"
 	"log"
 	"mygame/internal/game"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type DudeRenderer struct {
@@ -33,17 +31,4 @@ func (rdr *DudeRenderer) Draw(dude *game.Dude, screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(dude.Pos.X), float64(dude.Pos.Y))
 	screen.DrawImage(rdr.dudeImage, op)
-
-	if dude.IsShooting {
-		mx, my := ebiten.CursorPosition()
-
-		// compute dude center
-		w := rdr.dudeImage.Bounds().Dx()
-		h := rdr.dudeImage.Bounds().Dy()
-		dx := float64(dude.Pos.X) + float64(w)/2
-		dy := float64(dude.Pos.Y) + float64(h)/2
-
-		// draw red line using vector.StrokeLine (no anti-aliasing)
-		vector.StrokeLine(screen, float32(dx), float32(dy), float32(mx), float32(my), 2.0, color.RGBA{R: 255, G: 0, B: 0, A: 255}, false)
-	}
 }
